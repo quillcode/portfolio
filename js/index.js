95df38d9e8639e5522ad78135856c4af7c9efc80
Ahmad Zubair Zahid <ahmad.zubair.amar@gmail.com>
2019-06-08 14:17:19 +0430

    duplicate item removed

--- js/index.js
+++ js/index.js
@@ -6,20 +6,20 @@ btn_jadwal.addEventListener('click', (event) => {
     displayDescription(btn_jadwal, btn_schedual);
 });
 
-btn_schedual.addEventListener('click', (event)=>{
+btn_schedual.addEventListener('click', (event) => {
     displayDescription(btn_schedual, btn_jadwal);
 });
 
-function displayDescription(target, old_target, desc, old_desc){
+function displayDescription(target, old_target, desc, old_desc) {
     if (!target.classList.contains('select')) {
         target.classList.add('select');
         old_target.classList.remove('select');
-    }
 
-    descs.forEach((desc)=>{
-        desc.classList.toggle('visible');
-    });
-    pcs.forEach((pc)=>{
-        pc.classList.toggle('visible');
-    });    
+        descs.forEach((desc) => {
+            desc.classList.toggle('visible');
+        });
+        pcs.forEach((pc) => {
+            pc.classList.toggle('visible');
+        });
+    }
 }
